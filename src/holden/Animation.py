293dd18910bdb885ed9bc99bf7da21e42bc9d509
Brent Yi <yibrenth@gmail.com>
2023-10-30 14:09:29 -0700

    Remove umath_tests, which is unavailable in newer versions of numpy

--- src/holden/Animation.py
+++ src/holden/Animation.py
@@ -1,7 +1,6 @@
 import operator
 
 import numpy as np
-import numpy.core.umath_tests as ut
 
 import AnimationStructure
 from Quaternions import Quaternions
@@ -358,7 +357,7 @@ def transforms_multiply(t0s, t1s):
         together
     """
 
-    return ut.matrix_multiply(t0s, t1s)
+    return np.einsum("fjab,fjbc->fjac", t0s, t1s)
 
 
 def transforms_inv(ts):
--- src/holden/Quaternions.py
+++ src/holden/Quaternions.py
@@ -286,8 +286,7 @@ class Quaternions:
 
         if len(self.shape) == 1:
 
-            import numpy.core.umath_tests as ut
-            system = ut.matrix_multiply(self.qs[:, :, np.newaxis], self.qs[:, np.newaxis, :]).sum(axis=0)
+            system = np.einsum("bij,bjk->bik", self.qs[:, :, np.newaxis], self.qs[:, np.newaxis, :]).sum(axis=0)
             w, v = np.linalg.eigh(system)
             qiT_dot_qref = (self.qs[:, :, np.newaxis] * v[np.newaxis, :, :]).sum(axis=1)
             return Quaternions(v[:, np.argmin((1.-qiT_dot_qref**2).sum(axis=0))])
